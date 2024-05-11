fce23c44e2043f50794618a048e2e0d1f0618c03
m51k <mekail@gmail.com>
2024-05-11 12:22:33 +0500

    fixed rectangle trails in scrot

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -60,7 +60,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *screenshotcmd[]  = { "scrot", "-s", "/home/nep/Pictures/screenshots/%Y-%m-%d-%s_$wx$h.jpg", NULL };
+static const char *screenshotcmd[]  = { "scrot", "-s", "--line mode=edge", "/home/nep/Pictures/screenshots/%Y-%m-%d-%s_$wx$h.jpg", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
