ca3ae191cdb3f71927d9a607dad736e3c19cc2b6
m51k <mekail@gmail.com>
2025-02-28 03:19:10 +0530

    fix git abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ end
 if status is-interactive
 	# commands to run in interactive session
 	set -U fish_greeting ""
-	abbr -a dotfiles 'git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+	abbr -a dotfiles '/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 	set -gx EDITOR nvim
 	set -gx MANPAGER "nvim +Man!"
 	zoxide init fish | source
