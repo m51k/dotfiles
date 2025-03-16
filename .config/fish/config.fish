38104b1b9034257077fb111344d157734f6ebe5f
m51k <mekail@gmail.com>
2025-03-17 03:35:52 +0530

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,9 +6,7 @@ end
 if status is-interactive
 	# commands to run in interactive session
 	set -U fish_greeting ""
-	abbr -a dotfiles '/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
 	set -gx EDITOR nvim
 	set -gx MANPAGER "nvim +Man!"
 	zoxide init fish | source
-	alias cd z
 end
